ba3c28e69d764bd39b805e2306c5f563ee262af2
DESKTOP-U22TG9R\Kent Stringer <stringer_k@hotmail.com>
2018-08-15 11:06:37 -0600

    Getting Random DnD encounter to work. Not Finished

---
+++ Community-Discord-BOT-master/CommunityBot/Features/DnDHelper/Monster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityBot.Features.DnDHelper
+{
+    public class Monster
+    {
+        public string name { get; set; }
+        public string alignment { get; set; }
+        public string challenge { get; set; }
+        public string size { get; set; }
+        public string xp { get; set; }
+        public string type { get; set; }
+        public string page { get; set; }
+        public Dictionary<string,string> environment { get; set; }
+    }
+}
---
+++ Community-Discord-BOT-master/CommunityBot/Modules/DnD.cs
@@ -0,0 +1,20 @@
+﻿using CommunityBot.Features.DnDHelper;
+using CommunityBot.Features.GlobalAccounts;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityBot.Modules
+{
+    public class DnD : ModuleBase<SocketCommandContext>
+    {
+        [Command("Encounter"), Remarks("Shows how many Miunies you have")]
+        [Alias("Fight", "fight")]
+        public async Task GenerateFight(string cr)
+        {
+            await Context.Channel.SendMessageAsync(RandomEncounter.GetEncounter(cr));
+        }
+    }
+}
