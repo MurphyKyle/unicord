ef1ac8300fec564574066da8e0557f958f6687a0
DESKTOP-U22TG9R\Kent Stringer <stringer_k@hotmail.com>
2018-08-22 15:21:54 -0600

    Add treasure calcuations classes

---
+++ Community-Discord-BOT-master/CommunityBot/Features/DnDHelper/Models/NumberRange.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityBot.Features.DnDHelper.Models
+{
+    public class NumberRange
+    {
+        public int LowEnd { get; set; }
+        public int HighEnd { get; set; }
+
+        public static bool operator ==(NumberRange obj1, int obj2)
+        {
+            if (obj2 >= obj1.LowEnd && obj2 <= obj1.HighEnd)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(NumberRange obj1, int obj2)
+        {
+            if (obj2 >= obj1.LowEnd && obj2 <= obj1.HighEnd)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public NumberRange(int low, int high)
+        {
+            LowEnd = low;
+            HighEnd = high;
+        }
+    }
+}
---
+++ Community-Discord-BOT-master/CommunityBot/Features/DnDHelper/TreasureGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityBot.Features.DnDHelper
+{
+    public static class TreasureGenerator
+    {
+
+    }
+}
